bd2c9bb21f2dc93081337184fa0064da940d2020
AlirezaShah-dev <shahgptbot@gmail.com>
2025-11-29 11:38:10 +0330

    Configure for trybear.github.io user site

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  base: "/trybear.github.io/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
